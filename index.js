bb546b29222cc3cd823adfd1939cfae4dfe1490e
hayvanmavi123 <hayvanmavi123@gmail.com>
2026-08-16 07:43:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
-client.on('ready', () => {
-  console.log(`Bot ${client.user.tag} olarak giriş yaptı!`);
+client.once('ready', () => {
+    console.log(`Bot ${client.user.tag} olarak giriş yaptı!`);
 });
 
-client.login(process.env.DISCORD_TOKEN);
+client.login("4c42baf286832d640a361f72e09f1d781d1dcd27af26afd80b594aa70a5639fa");
