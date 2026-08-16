5797c5bec0936a4d38b56093c9c1c2ae898b11dc
hayvanmavi123 <hayvanmavi123@gmail.com>
2026-08-16 07:10:17 +0300

    Create index.js
    .

---
+++ index.js
@@ -0,0 +1,8 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.on('ready', () => {
+  console.log(`Bot ${client.user.tag} olarak giriş yaptı!`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
